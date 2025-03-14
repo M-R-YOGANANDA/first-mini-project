bed98228e91e1ee31a73356a523db9a35857a610
M-R-YOGANANDA <yoganandamr7002@gmail.com>
2025-03-14 14:13:10 +0530

    Create opencv_camera.py

---
+++ opencv_camera.py
@@ -0,0 +1,18 @@
+#before running this code in python install opencv by using pip
+#run this in your command propt 
+#"pip install opencv"
+
+
+
+import cv2
+def capture():
+    cap=cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("error web cam is not opened")
+        exit()
+    iscaptured,frame=cap.read()
+    if iscaptured:
+        return frame
+    cap.release()
+    cv2.destroyAllWindows()
+capture()
